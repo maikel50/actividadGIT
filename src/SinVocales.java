e86e11ecf1697748c7816c672f37369020170c6f
Azeskiv <acasadojareno@gmail.com>
2023-05-22 12:42:46 +0200

    Sin vocales ouu yeah

--- src/SinVocales.java
+++ src/SinVocales.java
@@ -1,4 +1,10 @@
-
+import java.util.*;
 public class SinVocales {
-
+public static Scanner sc = new Scanner(System.in);
+public static void main(String[] args) {
+	System.out.println("Introduce una palabra");
+	String string = sc.next();
+	string = string.replaceAll("[AaEeIiOoUu]", "");
+	System.out.println(string);
+}
 }
